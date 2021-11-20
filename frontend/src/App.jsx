1356c5cc2158a1644414570dc6cfecf6e3f6ba9d
Roya Chobineh <roya.chbn@gmail.com>
2021-11-20 11:02:59 -0500

    voice model added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 import UserList from './components/UserList';
+import tf from '@tensorflow/tfjs';
+import * as speechCommands from '@tensorflow-models/speech-commands';
 
 
 function App() {
+  async function createModel() {
+    const URL = "http://localhost:3001/audio-model/";
+    const checkpointURL = URL + "model.json"; // model topology
+    const metadataURL = URL + "metadata.json"; // model metadata
+
+    const recognizer = speechCommands.create(
+        "BROWSER_FFT", // fourier transform type, not useful to change
+        undefined, // speech commands vocabulary feature, not useful for your models
+        checkpointURL,
+        metadataURL);
+
+    // check that model and metadata are loaded via HTTPS requests.
+    await recognizer.ensureModelLoaded();
+
+    return recognizer;
+} 
+async function init() {
+  const recognizer = await createModel();
+  const classLabels = recognizer.wordLabels(); // get class labels
+  // const labelContainer = document.getElementById("label-container");
+
+  // listen() takes two arguments:
+  // 1. A callback function that is invoked anytime a word is recognized.
+  // 2. A configuration object with adjustable fields
+  recognizer.listen(result => {
+      const scores = result.scores; // probability of prediction for each class
+      // render the probability scores per class
+      console.log(scores);
+  }, {
+      includeSpectrogram: true, // in case listen should return result.spectrogram
+      probabilityThreshold: 0.75,
+      invokeCallbackOnNoiseAndUnknown: true,
+      overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
+  });
+
+  // Stop the recognition in 5 seconds.
+  // setTimeout(() => recognizer.stopListening(), 5000);
+}
   return (
     <div className="App">
-       <h1> super exam </h1>
-       <UserList /> 
+       {/* <h1> super exam </h1>
+       <UserList />  */}
+       <button onClick={init}>Start</button>
 
     </div>
   );
