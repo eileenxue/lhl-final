6c1d4958c49deffec6834780c195df16169121f5
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2021-11-20 14:31:12 -0500

    pg, pg-native

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const db = require('./db');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -27,7 +27,7 @@ router.get('/', function(req, res, next) {
       password: "123",
     },
   ];
-
+  res.json(users);
   res.send('TESTTESTESTRST')
 });
 
