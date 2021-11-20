3c10e43b3f8514d242e8d6f42cf88ac3806a9e02
Roya Chobineh <roya.chbn@gmail.com>
2021-11-20 02:43:56 -0500

    db updated

--- backend/db/schema/05_appointment.sql
+++ backend/db/schema/05_appointment.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS appointments CASCADE;
 CREATE TABLE "appointments" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "test_id" INTEGER REFERENCES tests(id) ON DELETE CASCADE,
   "is_proctor" BOOLEAN,
   "start_date" DATE
 );
--- backend/db/seeds/05_appointments.sql
+++ backend/db/seeds/05_appointments.sql
@@ -1,6 +1,6 @@
 INSERT INTO
-appointments(user_id, is_proctor, start_date)
+appointments(user_id, test_id, is_proctor, start_date)
 VALUES
-(1, True, '2021-12-03'),
-(2, False, '2021-12-03'),
-(3, False, '2021-12-20');
+(1, 1, True, '2021-12-03'),
+(2, 1, False, '2021-12-03'),
+(3, 2, False, '2021-12-20');
