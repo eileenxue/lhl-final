c61d7501e348bb96e7dbc8acf4c1353287629756
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2021-11-21 23:58:37 -0500

    resave the correct version

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,39 @@
 const PORT = process.env.PORT || 3005;
 const express = require('express');
+const app = express();
+const http = require("http");
+const cors = require("cors"); 
+const { Server } = require("socket.io"); 
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3002", // pointing to the front url
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    // console.log(`server part message ${data.key}`);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -7,7 +41,9 @@ const logger = require('morgan');
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
-var app = express();
+
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,7 +66,7 @@ app.post("/api/login") // why not get again ????/ insert and return back the use
 app.post("/api/register")
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
