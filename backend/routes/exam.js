edf3ff192db51640319d225755354c6b69646cd6
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2021-12-01 10:32:54 -0500

    fix proctor page wording

--- backend/routes/exam.js
+++ backend/routes/exam.js
@@ -7,9 +7,10 @@ module.exports = (db) => {
   router.get("/:appointment_id", (req, res) => {
     console.log("/exam/id backend", req.params.appointment_id);
     db.query(
-    `select users.first_name as first_name, users.id as student_id, appointments.id as id
+    `select users.first_name as first_name, users.id as student_id, appointments.id as id, tests.type
       from appointments 
       join users on users.id = appointments.student_id
+      join tests on tests.id = appointments.test_id
      where appointments.id = '${req.params.appointment_id}';`
     // ).then((result) => console.log("got it from the backend +++++ finish exam"));
     ).then((result) => res.status(200).json({ test: result.rows }));
